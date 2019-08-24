e5f176ce7a2565d48e9ef3332fcc18b72072e49d
GeorgiSwiftDeveloper <malkhasyangeorge@gmail.com>
2019-08-23 17:30:01 -0700

    Start MapView

--- WorkingMapKit/ViewController.swift
+++ WorkingMapKit/ViewController.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+ 
 
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        <#code#>
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        <#code#>
+    }
+}
